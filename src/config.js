e0046f161d88b382c212923ccd5ecf2954fa57a6
Olumide <127313839+fakowajo123@users.noreply.github.com>
2025-12-09 22:05:56 +0000

    Remove port from API_HOST URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // incident-frontend/src/config.js
-export const API_HOST = 'http://k8s-incident-incident-a59589fc56-1030006196.us-east-1.elb.amazonaws.com:8000';
+export const API_HOST = 'http://k8s-incident-incident-a59589fc56-1030006196.us-east-1.elb.amazonaws.com';
